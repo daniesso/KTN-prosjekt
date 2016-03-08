0140bda9da018b1fbff86fdef756246ca8fb2e70
stianste <stiansteinbakken17@gmail.com>
2016-03-08 14:39:33 +0100

    Add threading to Client.py
    I have not got around to testing it yet. How can I test it localy on my
    own computer? Do I have to set up my own server?

--- Client/Client.py
+++ Client/Client.py
@@ -1,22 +1,18 @@
 # -*- coding: utf-8 -*-
 import socket
-from MessageReceiver import MessageReceiver
-from MessageParser import MessageParser
+from threading import *
+#from MessageReceiver import MessageReceiver
+#from MessageParser import MessageParser
 
 class Client:
-    """
-    This is the chat client class
-    """
 
     def __init__(self, host, server_port):
-        """
-        This method is run when creating a new Client object
-        """
 
         # Set up the socket connection to the server
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.host = host
+        self.server_port = server_port
         
-        # TODO: Finish init process with necessary code
         self.run()
 
     def run(self):
@@ -27,17 +23,18 @@ class Client:
         # TODO: Handle disconnection
         pass
 
-    def receive_message(self, message):
-        # TODO: Handle incoming message
-        pass
+    def receive_message(self):
+        while True:
+            message = clientSocket.recvfrom(1024)
+            print(message)
 
     def send_payload(self, data):
-        # TODO: Handle sending of a payload
-        pass
+        clientSocket.sendto(data, self.host, self.server_port)
+
+    def take_and_send_input():
+        while True:
+            send_payload(input(": "))
         
-    # More methods may be needed!
-
-
 if __name__ == '__main__':
     """
     This is the main method and is executed when you type "python Client.py"
@@ -45,4 +42,7 @@ if __name__ == '__main__':
 
     No alterations are necessary
     """
-    client = Client('localhost', 9998)
+    client = Client('localhost', 1337)
+    client.run()
+    Thread(target=take_and_send_input).start()
+    Thread(target=recieve_message)
